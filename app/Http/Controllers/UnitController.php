745aa18aeec498bf146d6367967bf706bf3bcb9d
thanglongdong <104827372+thanglongdong@users.noreply.github.com>
2022-05-16 20:00:51 +1000

    Add files via upload
    Unit controller

---
+++ app/Http/Controllers/UnitController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Unit;
+use Illuminate\Http\Request;
+use function PHPUnit\Framework\isEmpty;
+
+class UnitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if(session('staff')=='0')
+            return redirect('main');
+        //
+        $units = Unit::all();
+        //return $units;
+        return view('unit',compact('units'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Unit  $unit
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Unit $unit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Unit  $unit
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Unit $unit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Unit  $unit
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Unit $unit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Unit  $unit
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Unit $unit)
+    {
+        //
+    }
+
+    public function search()
+    {
+        //return view('search');
+    }
+}
